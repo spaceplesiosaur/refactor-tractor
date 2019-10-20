19fa176752ad46bbc0f4ed4971e586af8868f98c
Allie McCarthy <mccarthy.allie@gmail.com>
2019-10-20 10:29:06 -0600

    Add SleepRepo in a separate branch to be checked over

--- src/SleepRepo.js
+++ src/SleepRepo.js
@@ -9,9 +9,8 @@ class SleepRepo {
       return totalQuality;
     }, 0) / this.sleepData.length).toFixed(1));
   }
-
-  returnAboveAverageSleepers(week) {
-    let dataByUser = this.sleepData.reduce((arr, user) => {
+  returnDataByUser() {
+    return this.sleepData.reduce((arr, user) => {
       if (!arr[user.userID - 1]) {
         arr[user.userID - 1] = [user];
       } else {
@@ -19,11 +18,34 @@ class SleepRepo {
       }
       return arr;
     }, []);
+  }
 
-    let avgSleepQualityPerUser = dataByUser.map(user => [...user].splice(-7 * week, 7)).map(user => user.reduce((totalQuality, day) => {
-      totalQuality += day.sleepQuality;
+  returnAvgSleepDataPerUser(week, relevantProperty) {
+    let dataByUser = this.returnDataByUser()
+
+    return dataByUser.map(user => [...user].splice(-7 * week, 7)).map(user => user.reduce((totalQuality, day) => {
+      totalQuality += day[relevantProperty];
       return totalQuality;
-    }, 0)).map(user => Number((user / 7).toFixed(2)));
+    }, 0))
+  }
+
+  returnAboveAverageSleepers(week) {
+    let dataByUser = this.returnDataByUser();
+    // let dataByUser = this.sleepData.reduce((arr, user) => {
+    //   if (!arr[user.userID - 1]) {
+    //     arr[user.userID - 1] = [user];
+    //   } else {
+    //     arr[user.userID - 1].push(user);
+    //   }
+    //   return arr;
+    // }, []);
+
+    // let avgSleepQualityPerUser = dataByUser.map(user => [...user].splice(-7 * week, 7)).map(user => user.reduce((totalQuality, day) => {
+    //   totalQuality += day.sleepQuality;
+    //   return totalQuality;
+    // }, 0)).map(user => Number((user / 7).toFixed(2)));
+
+    let avgSleepQualityPerUser = this.returnAvgSleepDataPerUser(dataByUser, 'sleepQuality').map(user => Number((user / 7).toFixed(2)));
 
     let goodSleepers = [];
     avgSleepQualityPerUser.forEach((user, index) => {
@@ -41,19 +63,24 @@ class SleepRepo {
   }
 
   returnWeeklyLongestSleepers(week) {
-    let dataByUser = this.sleepData.reduce((arr, user) => {
-      if (!arr[user.userID - 1]) {
-        arr[user.userID - 1] = [user];
-      } else {
-        arr[user.userID - 1].push(user);
-      }
-      return arr;
-    }, []);
+    let dataByUser = this.returnDataByUser();
 
-    let avgSleepHoursPerUser = dataByUser.map(user => [...user].splice(-7 * week, 7)).map(user => user.reduce((totalHours, day) => {
-      totalHours += day.hoursSlept;
-      return totalHours;
-    }, 0));
+    // let dataByUser = this.sleepData.reduce((arr, user) => {
+    //   if (!arr[user.userID - 1]) {
+    //     arr[user.userID - 1] = [user];
+    //   } else {
+    //     arr[user.userID - 1].push(user);
+    //   }
+    //   return arr;
+    // }, []);
+
+    let avgSleepHoursPerUser = this.returnAvgSleepDataPerUser(dataByUser, 'hoursSlept')
+
+    // let avgSleepHoursPerUser = dataByUser.map(user => [...user].splice(-7 * week, 7)).map(user => user.reduce((totalHours, day) => {
+    //   totalHours += day.hoursSlept;
+    //   return totalHours;
+    // }, 0));
+
     return [Math.max(...avgSleepHoursPerUser), avgSleepHoursPerUser.indexOf(Math.max(...avgSleepHoursPerUser)) + 1];
   }
 }
